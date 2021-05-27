caf4cf063266d45c27ccaf877edfb0639c27c1a4
noordseoostende <voropayev1@mail.ru>
2021-05-27 17:13:48 +0200

    a postman reaction is correct

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+  import { UserModule } from './user/user.module';
+  import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, 
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'db',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'admin',
+      autoLoadEntities: true,
+      synchronize: true,
+    }), AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
