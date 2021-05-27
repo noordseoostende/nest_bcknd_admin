d779526c15211f9017b6095a3636d708d2cf174a
noordseoostende <voropayev1@mail.ru>
2021-05-27 21:33:10 +0200

    inloggen is mogelijk (postman)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,7 @@
-import {Body, Controller, Post} from '@nestjs/common';
+import {BadRequestException, Body, Controller, NotFoundException, Post} from '@nestjs/common';
 import {UserService} from "../user/user.service";
+import * as bcrypt from 'bcryptjs';
+import {RegisterDto} from "./models/register.dto";
 
 @Controller()
 export class AuthController {
@@ -8,7 +10,34 @@ export class AuthController {
     }
 
     @Post('register')
-    async register(@Body() body) {
-        return this.userService.create(body);
+    async register(@Body() body: RegisterDto) {
+        if (body.password !== body.password_confirm) {
+            throw new BadRequestException('Wachtwoord ongeldig is!');
+        }
+        const hashed = await bcrypt.hash(body.password, 12);
+
+        return this.userService.create({
+            first_name: body.first_name,
+            last_name: body.last_name,
+            email: body.email,
+            password: hashed,
+        });
+    }
+
+    @Post('login')
+    async login(
+        @Body('email') email: string,
+        @Body('password') password: string
+    ) {
+        const user = await this.userService.findOne({email});
+
+        if (!user) {
+            throw new NotFoundException('Gebruiker was niet gevonden');
+        }
+
+        if (!await bcrypt.compare(password, user.password)) {
+            throw new BadRequestException('Ongeldige gegevens');
+        }
+        return user;
     }
 }
