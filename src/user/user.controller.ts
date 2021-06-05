60d4a39f577e6e8186dc724ecf0fe6aa8b6c553b
noordseoostende <voropayev1@mail.ru>
2021-06-05 17:25:14 +0200

    permission of permissions

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -18,6 +18,7 @@ import {AuthGuard} from "../auth/auth.guard";
 import {UserUpdateDto} from "./models/user-update.dto";
 import {AuthService} from "../auth/auth.service";
 import {Request} from "express";
+import {HasPermission} from "../permission/has-permission.decorator";
 
 @UseInterceptors(ClassSerializerInterceptor)
 @UseGuards(AuthGuard)
@@ -32,12 +33,14 @@ export class UserController {
     }
 
     @Get()
+    @HasPermission('users')
     async all(@Query('page') page = 1){
         return this.userService.paginate(page, ['role']);
     }
 
 
     @Post()
+    @HasPermission('users')
     async create(@Body() body: UserCreateDto): Promise<User> {
         const password = await bcrypt.hash('1234', 12);
 
@@ -51,6 +54,7 @@ export class UserController {
     }
 
     @Get(':id')
+    @HasPermission('users')
     async get(@Param('id') id: number) {
         return this.userService.findOne({id}, ['role']);
     }
@@ -86,6 +90,7 @@ export class UserController {
     }
 
     @Put(':id')
+    @HasPermission('users')
     async update(
         @Param('id') id: number,
         @Body() body: UserUpdateDto
@@ -102,6 +107,7 @@ export class UserController {
     }
 
     @Delete(':id')
+    @HasPermission('users')
     async delete(@Param('id') id: number) {
         return this.userService.delete(id);
     }
